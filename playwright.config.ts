1d1a0434ae403f7b3300c1b88d48eb398200a619
Salvador Manzanera <salvador.manzanera@externos.allianz.es>
2026-03-03 11:28:46 +0100

    actualiza la configuración de CI/CD para usar npm ci y elimina la configuración de pruebas para Mobile Safari

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
   ],
   webServer: {
     command: 'npm run dev',
